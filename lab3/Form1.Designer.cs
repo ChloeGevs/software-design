56e36beaec463d2344bf76ed661212cfb5cf6570
ChloeGevs <c.geverola.557546@umindanao.edu.ph>
2026-02-06 02:46:43 +0800

    Completed Lab 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,65 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBoxBooks = new ListBox();
+            btnShowInfo = new Button();
+            btnClear = new Button();
+            SuspendLayout();
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(156, 36);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(475, 284);
+            listBoxBooks.TabIndex = 0;
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(156, 337);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(283, 49);
+            btnShowInfo.TabIndex = 1;
+            btnShowInfo.Text = "Show";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnShowInfo_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(455, 337);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(176, 49);
+            btnClear.TabIndex = 2;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnClear);
+            Controls.Add(btnShowInfo);
+            Controls.Add(listBoxBooks);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private ListBox listBoxBooks;
+        private Button btnShowInfo;
+        private Button btnClear;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,86 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Task 2: Polymorphic Behavior
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book[] inventory = new Book[]
+            {
+                new Magazine { Title = "National Geographic", IssueNumber = 202 },
+                new Ebook { Title = "C# Design Patterns", Author = "Steve Smith", FileSizeMB = 1.5 },
+                new Textbook { Title = "University Physics", Author = "Young & Freedman", Subject = "Science" },
+                new AudioBook { Title = "The Hobbit", Author = "J.R.R. Tolkien", Narrator = "Andy Serkis", DurationHours = 10.5 }
+            };
+
+            listBoxBooks.Items.Clear();
+
+            foreach (Book item in inventory)
+            {
+                listBoxBooks.Items.Add(item.GetInfo());
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listBoxBooks.Items.Clear();
+        }
+    }
+
+    // Task 1: Base Class
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public virtual string GetInfo()
+        {
+            return $"[Book] {Title} by {Author}";
+        }
+    }
+
+    // Task 1: Subclasses
+    public class Magazine : Book
+    {
+        public int IssueNumber { get; set; }
+        public override string GetInfo()
+        {
+            return $"[Magazine]: {Title}, Issue #{IssueNumber}";
+        }
+    }
+
+    public class Ebook : Book
+    {
+        public double FileSizeMB { get; set; }
+        public override string GetInfo()
+        {
+            return $"[Ebook]: {Title} ({FileSizeMB}MB)";
+        }
+    }
+
+    // Task 3: Student Challenge Subclasses
+    public class Textbook : Book
+    {
+        public string Subject { get; set; }
+        public override string GetInfo()
+        {
+            return $"[Textbook]: {Title}, Subject: {Subject}";
+        }
+    }
+
+    public class AudioBook : Book
+    {
+        public string Narrator { get; set; }
+        public double DurationHours { get; set; }
+        public override string GetInfo()
+        {
+            return $"[Audio]: {Title}, Narrator: {Narrator} ({DurationHours} hrs)";
+        }
+    }
 }
+
